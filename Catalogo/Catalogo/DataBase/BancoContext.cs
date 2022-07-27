16cfde29e36f5f484fb62e8ade0472bd83548a12
Jessica Alves <jessica@suasvendas.com>
2022-07-27 16:18:36 -0300

    Ok

---
+++ Catalogo/Catalogo/DataBase/BancoContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Catalogo.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalogo.DataBase
+{
+    public class BancoContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        public BancoContext()
+        {
+        }
+    }
+}
+
---
+++ Catalogo/Catalogo/DataBase/Constantes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Catalogo.DataBase
+{
+    public static class Constants
+    {
+        public const string NomeDoArquivo = "dbCatalogo.db3";
+
+        public static string CaminhoDoBanco
+        {
+            get
+            {
+                var caminhoBase = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return Path.Combine(caminhoBase, NomeDoArquivo);
+            }
+        }
+    }
+}
+
